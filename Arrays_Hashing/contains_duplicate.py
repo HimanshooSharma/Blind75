bffe111a6dcf32b989d1873061a01ee281699e18
Himanshoo <himanshoosharma@live.com>
2022-07-28 17:11:12 +0200

    First problem solved

---
+++ Arrays_Hashing/contains_duplicate.py
@@ -0,0 +1,31 @@
+"""
+https://leetcode.com/problems/contains-duplicate
+
+"""
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+
+        # Solution 01
+
+        # nums.sort()
+        # for i in range(0, len(nums)-1):
+        #     if nums[i] == nums[i+1]:
+        #         return True
+
+        # return False
+
+        # Solution 02
+
+        # nums_set = set(nums)
+
+        # return len(nums_set) != len(nums)
+
+        nums_set = set()
+
+        for num in nums:
+            if num in nums_set:
+                return True
+
+        return False
